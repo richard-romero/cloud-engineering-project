7ad773c743579d05709e3a833b661cecf35c29d9
richard-romero <richard.romero@ufl.edu>
2026-03-23 16:16:12 -0400

    feat(terraform): enhance user data script with system update and utility installation

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+set -eux
+
+# Update system
 dnf update -y
-dnf install nginx -y
+
+# Utilities
+dnf install -y nano
+
+# Install nginx
+dnf install -y nginx
 systemctl enable nginx
 systemctl start nginx
+
+# Install CloudWatch Agent
+dnf install -y amazon-cloudwatch-agent
+
+# Start agent (basic start for now)
+systemctl enable amazon-cloudwatch-agent
+systemctl start amazon-cloudwatch-agent
